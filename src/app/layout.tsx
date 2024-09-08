acef912b22e9f47f01fe23b8eeb71a36a73e1f05
tayoolaigbe <tayoolaigbe@outlook.com>
2024-09-08 17:16:09 +0100

    added the upload functionality with uploadthing and zod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
 				<main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
 					<div className="flex-1 flex flex-col h-full">{children}</div>
 				</main>
+				<Toaster />
 				<Footer />
 			</body>
 		</html>
