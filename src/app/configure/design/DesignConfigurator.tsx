204c2cfc0e10ae31c1e06cdde7241e239931a47f
tayoolaigbe <tayoolaigbe@outlook.com>
2024-09-14 22:13:38 +0100

    added the continue functionality on the DesignConfigurator and completed the DesignCofigurator component

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -13,7 +13,7 @@ import {
 	MATERIALS,
 	MODELS,
 } from '@/validators/option-validator';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Label } from '@/components/ui/label';
 import {
 	DropdownMenu,
@@ -22,7 +22,13 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { Check, ChevronsUpDown } from 'lucide-react';
+import { ArrowRight, Check, ChevronsUpDown } from 'lucide-react';
+import { BASE_PRICE } from '@/config/products';
+import { useUploadThing } from '@/lib/uploadthing';
+import { saveConfig as _saveConfig, SaveConfigArgs } from './actions';
+import { useToast } from '@/components/ui/use-toast';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 interface DesignConfiguratorProps {
 	configId: string;
@@ -35,6 +41,26 @@ const DesignConfigurator = ({
 	imageUrl,
 	imageDimensions,
 }: DesignConfiguratorProps) => {
+	const { toast } = useToast();
+	const router = useRouter();
+
+	const { mutate: saveConfig, isPending } = useMutation({
+		mutationKey: ['save-config'],
+		mutationFn: async (args: SaveConfigArgs) => {
+			await Promise.all([saveConfiguration(), _saveConfig]);
+		},
+		onError: () => {
+			toast({
+				title: 'Something went wrong',
+				description: 'There was an error on our end. Please try again.',
+				variant: 'destructive',
+			});
+		},
+		onSuccess: () => {
+			router.push(`/configure/preview?id=${configId}`);
+		},
+	});
+
 	const [options, setOptions] = useState<{
 		color: (typeof COLORS)[number];
 		model: (typeof MODELS.options)[number];
@@ -46,11 +72,95 @@ const DesignConfigurator = ({
 		material: MATERIALS.options[0],
 		finish: FINISHES.options[0],
 	});
+
+	const [renderedDimension, setRenderedDimension] = useState({
+		width: imageDimensions.width / 4,
+		height: imageDimensions.height / 4,
+	});
+
+	const [renderedPosition, setRenderedPosition] = useState({
+		x: 150,
+		y: 205,
+	});
+
+	const phoneCaseRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	const { startUpload } = useUploadThing('imageUploader');
+
+	async function saveConfiguration() {
+		try {
+			const {
+				left: caseLeft,
+				top: caseTop,
+				width,
+				height,
+			} = phoneCaseRef.current!.getBoundingClientRect();
+
+			const { left: containerLeft, top: containerTop } =
+				containerRef.current!.getBoundingClientRect();
+
+			const leftOffset = caseLeft - containerLeft;
+			const topOffset = caseTop - containerTop;
+
+			const actualX = renderedPosition.x - leftOffset;
+			const actualY = renderedPosition.y - topOffset;
+
+			const canvas = document.createElement('canvas');
+			canvas.width = width;
+			canvas.height = height;
+			const ctx = canvas.getContext('2d');
+
+			const userImage = new Image();
+			userImage.crossOrigin = 'anonymous';
+			userImage.src = imageUrl;
+			await new Promise(resolve => (userImage.onload = resolve));
+
+			ctx?.drawImage(
+				userImage,
+				actualX,
+				actualY,
+				renderedDimension.width,
+				renderedDimension.height
+			);
+
+			const base64 = canvas.toDataURL();
+			const base64Data = base64.split(',')[1];
+
+			const blob = base64ToBlob(base64Data, 'image/png');
+			const file = new File([blob], 'filename.png', { type: 'image/png' });
+
+			await startUpload([file], { configId });
+		} catch (err) {
+			toast({
+				title: 'Something went wrong',
+				description:
+					'There was a problem saving your config, please try again.',
+				variant: 'destructive',
+			});
+		}
+	}
+
+	// Convert base64 to image
+	function base64ToBlob(base64: string, mimeType: string) {
+		const byteCharacters = atob(base64);
+		const byteNumbers = new Array(byteCharacters.length);
+		for (let i = 0; i < byteCharacters.length; i++) {
+			byteNumbers[i] = byteCharacters.charCodeAt(i);
+		}
+		const byteArray = new Uint8Array(byteNumbers);
+		return new Blob([byteArray], { type: mimeType });
+	}
+
 	return (
 		<div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
-			<div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+			<div
+				ref={containerRef}
+				className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+			>
 				<div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831">
 					<AspectRatio
+						ref={phoneCaseRef}
 						ratio={896 / 1831}
 						className="pointer-events-none relative z-50 aspect-[896/1831] w-full"
 					>
@@ -75,6 +185,17 @@ const DesignConfigurator = ({
 						y: 205,
 						height: imageDimensions.height / 4,
 						width: imageDimensions.width / 4,
+					}}
+					onResizeStop={(_, __, ref, ___, { x, y }) => {
+						setRenderedDimension({
+							height: parseInt(ref.style.height.slice(0, -2)),
+							width: parseInt(ref.style.width.slice(0, -2)),
+						});
+						setRenderedPosition({ x, y });
+					}}
+					onDragStop={(_, data) => {
+						const { x, y } = data;
+						setRenderedPosition({ x, y });
 					}}
 					className="absolute z-20 border-[3px] border-primary"
 					lockAspectRatio
@@ -254,6 +375,39 @@ const DesignConfigurator = ({
 						</div>
 					</div>
 				</ScrollArea>
+
+				<div className="w-full px-8 h-16 bg-white">
+					<div className="h-px w-full bg-zinc-200" />
+					<div className="w-full h-full flex justify-end items-center">
+						<div className="w-full flex gap-6 items-center">
+							<p className="font-medium whitespace-nowrap">
+								{formatPrice(
+									(BASE_PRICE + options.finish.price + options.material.price) /
+										100
+								)}
+							</p>
+							<Button
+								isLoading={isPending}
+								disabled={isPending}
+								loadingText="Saving"
+								onClick={() =>
+									saveConfig({
+										configId,
+										color: options.color.value,
+										finish: options.finish.value,
+										material: options.material.value,
+										model: options.model.value,
+									})
+								}
+								size="sm"
+								className="w-full"
+							>
+								Continue
+								<ArrowRight className="h-4 w-4 ml-1.5 inline" />
+							</Button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
